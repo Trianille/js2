f8d18ffab078ea2887e38884a939a8c7d427db69
ivan <nekofurs@gmail.com>
2018-08-20 03:21:17 +0700

    Added 4th lesson 1st task
    Without styling

---
+++ less4/javascript/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const app = new Vue({
+        el: '#tabs-app',
+        data() {
+            return {
+                lang: 'cats',
+            }
+        }
+    })
+});
